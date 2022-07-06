843a79b12326bed846e60158a786821d58c7da46
Development Machine <68969699+Dev3lopmentM4chine@users.noreply.github.com>
2022-07-06 20:29:52 -0300

    Update script.js

--- CursoemVideo/JavaScript/Módulo F/Exercicíos/Ex01/assets/script.js
+++ CursoemVideo/JavaScript/Módulo F/Exercicíos/Ex01/assets/script.js
@@ -10,18 +10,19 @@ const n = document.getElementById('num')
 const btn1 = document.getElementById('btn-num')
 const multmedia = document.getElementById('multmedia')
 const btn2 = document.getElementById('btn-area')
+btn1.addEventListener('click', validation)
+
 const num = []
 
-btn1.addEventListener('click', validar)
 
-function validar() {
+function validation() {
     if (Number(n.value) <= 0 || Number(n.value) > 100) {
         return alert(`[Erro:${Number(n.value)}]Informe um número valído!`)
     } else {
-        return carregar()
+        return Addnumber()
     }
 }
-function carregar() {
+function Addnumber() {
     let opt = document.createElement('option')
     opt.text = `Número ${Number(n.value)} adicionado!`
     multmedia.appendChild(opt)
