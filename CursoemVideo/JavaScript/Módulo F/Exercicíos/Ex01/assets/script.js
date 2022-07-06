124b961de88573fc49d8c27b54655648425b59c8
Development Machine <68969699+Dev3lopmentM4chine@users.noreply.github.com>
2022-07-06 20:24:47 -0300

    update

--- CursoemVideo/JavaScript/Módulo F/Exercicíos/Ex01/assets/script.js
+++ CursoemVideo/JavaScript/Módulo F/Exercicíos/Ex01/assets/script.js
@@ -1,3 +1,11 @@
+/* 
+    *funcionalidades:
+        -adicionar valores ao array
+        -ação do botão 'finalizar': mostrar o resultado na div 'num'
+        -ação de reset na div 'result' quando clicar no 'btn-num' se ja tiver sido gerado uma requisição de resultado na div 'result'.
+        -conteudo do campo de resultado:div 'result'
+*/
+
 const n = document.getElementById('num')
 const btn1 = document.getElementById('btn-num')
 const multmedia = document.getElementById('multmedia')
@@ -8,18 +16,13 @@ btn1.addEventListener('click', validar)
 
 function validar() {
     if (Number(n.value) <= 0 || Number(n.value) > 100) {
-        return console.log(`[Erro:${Number(n.value)}]Informe um número valído!`)
+        return alert(`[Erro:${Number(n.value)}]Informe um número valído!`)
     } else {
         return carregar()
     }
 }
 function carregar() {
-    for (let c = 1; c >= num.length; c--) {
-        num[num.length] = Number(n.value)
-    }
     let opt = document.createElement('option')
     opt.text = `Número ${Number(n.value)} adicionado!`
     multmedia.appendChild(opt)
-    console.log(num)
-    console.log(num.length)
 }
