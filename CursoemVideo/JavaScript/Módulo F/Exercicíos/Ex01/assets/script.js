0d01f95e3aff855046e78591b5dafdd5c2103fdf
Development Machine <68969699+Dev3lopmentM4chine@users.noreply.github.com>
2022-07-07 15:17:26 -0300

    update

--- CursoemVideo/JavaScript/Módulo F/Exercicíos/Ex01/assets/script.js
+++ CursoemVideo/JavaScript/Módulo F/Exercicíos/Ex01/assets/script.js
@@ -1,29 +1,20 @@
 /* 
-    *funcionalidades:
-        -adicionar valores ao array
-        -ação do botão 'finalizar': mostrar o resultado na div 'num'
-        -ação de reset na div 'result' quando clicar no 'btn-num' se ja tiver sido gerado uma requisição de resultado na div 'result'.
-        -conteudo do campo de resultado:div 'result'
+    *Funcionalidades do script-SEGUIR ORDEM ABAIXO:
+
+        -Validação de dados ao clicar no botão.
+        
+        -Adicionar valores ao array.
+        
+        -Adicionar valores ao select.
+        
+        -Ação do botão 'finalizar': mostrar o resultado na div 'num'.
+        
+        -Conteudo do campo de resultado:div 'result'.
+            *Total de numeros adicionados.
+            *O mair e o menor numero adicionados.
+            *Soma total dos números adicionados.
+            *A média total do números adicionados.
+        
+            -Ação de reset na div 'result' quando clicar no 'btn-num' se ja tiver sido gerado um
+        resultado.
 */
-
-const n = document.getElementById('num')
-const btn1 = document.getElementById('btn-num')
-const multmedia = document.getElementById('multmedia')
-const btn2 = document.getElementById('btn-area')
-btn1.addEventListener('click', validation)
-
-const num = []
-
-
-function validation() {
-    if (Number(n.value) <= 0 || Number(n.value) > 100) {
-        return alert(`[Erro:${Number(n.value)}]Informe um número valído!`)
-    } else {
-        return Addnumber()
-    }
-}
-function Addnumber() {
-    let opt = document.createElement('option')
-    opt.text = `Número ${Number(n.value)} adicionado!`
-    multmedia.appendChild(opt)
-}
