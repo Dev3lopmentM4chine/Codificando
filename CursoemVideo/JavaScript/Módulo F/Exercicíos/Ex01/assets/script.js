48d1a7ca973c5da5c18eb463b4f42bce64f260bc
Development Machine <68969699+Dev3lopmentM4chine@users.noreply.github.com>
2022-07-08 22:30:11 -0300

    update

--- CursoemVideo/JavaScript/Módulo F/Exercicíos/Ex01/assets/script.js
+++ CursoemVideo/JavaScript/Módulo F/Exercicíos/Ex01/assets/script.js
@@ -23,21 +23,42 @@ let getBtn1 = document.getElementById('btn-one')
 let getBtn2 = document.getElementById('btn-two')
 let getnum = document.getElementById('inp-num')
 let getAreaResult = document.getElementById('area-result')
+let getAreaResult2= document.getElementById('end-result-area')
+let menorNum = 0
+let maiorNum = 0
+let somaTotal = 0
+let mediaTotal = 0
 
 
 const numeros = []
 
 getBtn1.addEventListener('click', validacao)
+getBtn2.addEventListener('click', finalResult)
 
 function validacao() {
     let num = Number(getnum.value) //conversão de tipo primitivo
     if (num <= 0 || num > 100) {
         alert(`[Erro: Número invalido]`)
     } else {
-        if(numeros.indexOf(num) != -1){
+        if (numeros.indexOf(num) != -1) {
             alert(`[Erro: Número já inserido]`)
-        }else{
+        } else {
             numeros.push(num)
+            let opt = document.createElement('option')
+            opt.text = `Número ${num} adicionado!`
+            getAreaResult.appendChild(opt)
+            somaTotal = numeros.reduce((acc, numero) => acc + numero)
+            // somaTotal += num
+            mediaTotal = somaTotal/2
         }
     }
+}
+
+function finalResult(){
+    validacao()
+    getAreaResult2.innerHTML = `O total de números adicionados é ${numeros.length}`
+    getAreaResult2.innerHTML = `O maior número adicionado é ${maiorNum}`
+    getAreaResult2.innerHTML = `O menor número adicionado é ${menorNum}`
+    getAreaResult2.innerHTML = `A soma de todos os números é ${somaTotal}`
+    getAreaResult2.innerHTML = `A média de todos os números é ${mediaTotal}`
 }
